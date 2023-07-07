751c9f7b583a9eccc4bd5fb0e58f8677ec1262e3
heemankv <heemankv@gmail.com>
2023-07-07 15:16:49 +0530

    Solved: Medium : Maximum Subarray

---
+++ Maximum_Subarray/Maximum_Subarray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+
+        int maxi = INT_MIN; 
+        int sum = 0 ; 
+
+        for(const auto num:nums){
+            sum+= num;
+            
+            maxi = max(maxi, sum);
+
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxi;
+
+        
+    }
+};
